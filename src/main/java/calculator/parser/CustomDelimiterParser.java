60807bee36832d142d6d254eddb1665035b2059c
jintakkim <mine407603@gmail.com>
2025-10-25 16:15:38 +0900

    refactor: named capturing group을 적용

--- src/main/java/calculator/parser/CustomDelimiterParser.java
+++ src/main/java/calculator/parser/CustomDelimiterParser.java
@@ -6,15 +6,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class CustomDelimiterParser {
-    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("^//(.)\\\\n(.*)$");
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("^//(?<delimiter>.)\\\\n(?<numbers>.*)$");
 
     private CustomDelimiterParser() {}
 
     public static CustomDelimiterParsedInputContext parse(String text) {
         Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
         if (matcher.matches()) {
-            char customDelimiter = matcher.group(1).charAt(0);
-            String numbersText = matcher.group(2);
+            char customDelimiter = matcher.group("delimiter").charAt(0);
+            String numbersText = matcher.group("numbers");
             return new CustomDelimiterParsedInputContext(customDelimiter, numbersText);
         }
         return new CustomDelimiterParsedInputContext(null, text);
